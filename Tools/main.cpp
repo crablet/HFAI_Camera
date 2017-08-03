6bf2ada8104e52a8b600480aa223e0a3f59b8874
Crablet <love4uandgzfc@gmail.com>
2017-08-03 18:07:30 +0800

    Basic function completed.

--- Tools/main.cpp
+++ Tools/main.cpp
@@ -5,24 +5,28 @@
 using namespace cv;
 using namespace std;
 
-vector<Point> Points;
-bool Flag = false;
+Mat SrcImage, MaskImage;
+Point PrevPt(-1, -1);
 
-void OnMouseHandle(int event, int x, int y, int flags, void *param);
+static void OnMouseHandle(int event, int x, int y, int flags, void*);
 
 int main()
 {
-    Mat SrcImage = imread("1.jpg");
-    Mat TempImage;
+    SrcImage = imread("1.jpg");
+    imshow("In", SrcImage);
+
+    Mat TempImage, GrayImage;
     SrcImage.copyTo(TempImage);
 
-    namedWindow("In");
-    setMouseCallback("In", OnMouseHandle, &SrcImage);
+    cvtColor(SrcImage, MaskImage, COLOR_BGR2GRAY);
+    cvtColor(MaskImage, GrayImage, COLOR_GRAY2BGR);
+
+    MaskImage = Scalar::all(0);
+
+    setMouseCallback("In", OnMouseHandle, nullptr);
 
     while (1)
     {
-        SrcImage.copyTo(TempImage);
-        imshow("In", TempImage);
         if (waitKey(0))
             break;
     }
@@ -30,34 +34,61 @@ int main()
     return 0;
 }
 
-void OnMouseHandle(int event, int x, int y, int flags, void *param)
+static void OnMouseHandle(int event, int x, int y, int flags, void*)
 {
-    Mat &Image = *(Mat*)param;
-    switch (event)
+    if (x < 0 || x >= SrcImage.cols || y < 0 || y >= SrcImage.rows)
+        return;
+    if (event == EVENT_LBUTTONUP || !(flags & EVENT_FLAG_LBUTTON))
     {
-    case EVENT_LBUTTONDOWN:
-        Flag = true;
-        break;
-    case EVENT_LBUTTONUP:
-        Flag = false;
-        Points.clear();
-        break;
-    case EVENT_MOUSEMOVE:
-        if (Flag)
+        PrevPt = Point(-1, -1);
+    }
+    else if (event == EVENT_LBUTTONDOWN)
+    {
+        PrevPt = Point(x, y);
+    }
+    else if (event == EVENT_MOUSEMOVE && (flags & EVENT_FLAG_LBUTTON))
+    {
+        Point Now(x, y);
+        if (PrevPt.x < 0)
         {
-            Points.push_back(Point(x, y));
-            cout << x << " " << y << endl;
-            for (size_t i = 0; i < Points.size() - 2; ++i)
-            {
-                if (Points[i] == Point(x, y))
-                {
-                    cout << "Get!" << endl;
-                    break;
-                }
-            }
+            PrevPt = Now;
         }
-        break;
-    default:
-        break;
+        line(MaskImage, PrevPt, Now, Scalar::all(255), 5);
+        line(SrcImage, PrevPt, Now, Scalar::all(255), 5);
+        PrevPt = Now;
+        imshow("In", SrcImage);
     }
 }
+
+//void OnMouseHandle(int event, int x, int y, int flags, void *param)
+//{
+//    Mat &Image = *(Mat*)param;
+//    switch (event)
+//    {
+//    case EVENT_LBUTTONDOWN:
+//        Flag = true;
+//        break;
+//    case EVENT_LBUTTONUP:
+//        Flag = false;
+//        //Points.clear();
+//        break;
+//    case EVENT_MOUSEMOVE:
+//        if (Flag)
+//        {
+//            Points.push_back(Point(x, y));
+//            cout << x << " " << y << endl;
+//            circle(Image, Point(x, y), 2, Scalar(0, 255, 0), -1);
+//            for (size_t i = 0; i < Points.size() - 2; ++i)
+//            {
+//                if (Points[i] == Point(x, y))
+//                {
+//                    cout << "Get!" << endl;
+//                    break;
+//                }
+//            }
+//        }
+//        break;
+//    default:
+//        break;
+//    }
+//}
